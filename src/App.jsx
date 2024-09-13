b0cf5956c8783e53452fb0aa9a7c555a4f7a48e1
Jyoti_Prakash <jyotiprakash751321@gmail.com>
2024-09-13 17:36:20 +0530

    added add & delete feature

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import ReadTodos from './components/ReadTodos'
+import AddTodo from './components/AddTodo'
+
+function App() {
+
+  return (
+    <>
+      <AddTodo />
+      <ReadTodos />
+    </>
+  )
+}
+
+export default App
